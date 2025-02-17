6450de6376d5ad708c80dc449045f61a8c8dc98f
Dimitar <dimitar.mandarliev@gmail.com>
2025-02-17 09:01:09 +0200

    Refactor database schema references to use new naming conventions and implement query functions for folder and file retrieval

--- src/app/f/[folderId]/page.tsx
+++ src/app/f/[folderId]/page.tsx
@@ -1,28 +1,9 @@
-import { eq } from "drizzle-orm";
 import DriveContents from "~/app/drive-contents";
-import { db } from "~/server/db";
 import {
-  files as filesSchema,
-  folders as foldersSchema,
-} from "~/server/db/schema";
-
-async function getAllParents(folderId: number) {
-  const parents = [];
-  let currentId: number | null = folderId;
-  while (currentId !== null) {
-    const folder = await db
-      .selectDistinct()
-      .from(foldersSchema)
-      .where(eq(foldersSchema.id, currentId));
-
-    if (!folder[0]) {
-      throw new Error("Parent folder not found");
-    }
-    parents.unshift(folder[0]);
-    currentId = folder[0]?.parent;
-  }
-  return parents;
-}
+  getAllParentsForFolder,
+  getFiles,
+  getFolders,
+} from "~/server/db/queries";
 
 export default async function GoogleDriveClone(props: {
   params: Promise<{ folderId: string }>;
@@ -33,22 +14,10 @@ export default async function GoogleDriveClone(props: {
     return <div>Invalid folder ID</div>;
   }
 
-  const foldersPromise = db
-    .select()
-    .from(foldersSchema)
-    .where(eq(foldersSchema.parent, parsedFolderId));
-
-  const filesPromise = db
-    .select()
-    .from(filesSchema)
-    .where(eq(filesSchema.parent, parsedFolderId));
-
-  const parentsPromise = getAllParents(parsedFolderId);
-
   const [folders, files, parents] = await Promise.all([
-    foldersPromise,
-    filesPromise,
-    parentsPromise,
+    getFolders(parsedFolderId),
+    getFiles(parsedFolderId),
+    getAllParentsForFolder(parsedFolderId),
   ]);
 
   return <DriveContents files={files} folders={folders} parents={parents} />;
